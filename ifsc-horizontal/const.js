451b1fc71143615f00afbc90b975f241092cb26a
Marcio Galli <taboca@gmail.com>
2012-05-16 12:44:30 -0300

    hor

---
+++ ifsc-horizontal/const.js
@@ -0,0 +1,23 @@
+
+ACONTECE_URL = "http://latinoware.org/pt-br/rss.xml";
+ACONTECE_URL= "http://www.eesc.usp.br/portal/index.php?option=com_content&view=section&layout=blog&id=10&Itemid=1&format=feed&type=rss";
+URL_DEFESAS = "http://www.ifsc.usp.br/defesas2_rss.php";
+URL_MIDIA =  "http://api.flickr.com/services/feeds/photos_public.gne?id=66295838@N03&lang=pt-br&format=";
+URL_SIDEBAR =  "http://api.flickr.com/services/feeds/photos_public.gne?id=66295838@N03&lang=pt-br&format=";
+//URL_GALERIA =  "http://api.flickr.com/services/feeds/photos_public.gne?tags=flower&lang=pt-br&format=";
+URL_GALERIA =  "http://api.flickr.com/services/feeds/photoset.gne?set=72157629279414302&nsid=76472067@N08&lang=pt-br";
+URL_GALERIA =  "http://api.flickr.com/services/feeds/photos_public.gne?id=62735326@N03&lang=pt-br&format=";
+//URL_GALERIA =  "http://localhost:8888/channel-store-images/localpictures";
+URL_DESTAQUES = "http://www.eesc.usp.br/portal/index.php?option=com_content&view=section&layout=blog&id=10&Itemid=1&format=feed&type=rss";
+URL_DESTAQUES = "http://latinoware.org/pt-br/rss.xml";
+URL_DESTAQUES = "http://twitter.com/statuses/user_timeline/sematron.rss?count=20";
+URL_TYPING = "http://olhardigital.uol.com.br/rss/ultimas_noticias.php";
+URL_WEATHER = "http://weather.yahooapis.com/forecastrss?w=449704&u=c";
+
+TEMPO_INICIO_MIDIA = 5000; 
+TEMPO_REFRESH_MIDIA = 20000; 
+TEMPO_REFRESH = 60000; 
+TEMPO_REFRESH_WEATHER = 90000; 
+
+API_KEY_GOOGLE = 'ABQIAAAAdxhnO5HfbyeQ6NXKl4uDOxSKoqW534mSMfFUlHEO4X_p_TS_PBSii6og5U1MCZLzH-8-J8KiwNK3Tg';
+
---
+++ ifsc-horizontal/index.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+   register("/main/topo", "cabecalho", "./header/index.html", iframeTemplate);
+   register("/main/ladoe", "meio", "./twitter/scinfor.html", iframeTemplate);
+   register("/main/ladod", "mid", "./ifsc/grafico.html", iframeTemplate);
+   register("/main/fundo", "clima", "./typing/index.html", iframeTemplate);
+   compile();   
+   setTimeout('startEngine()',5000);
+});
+
+function startEngine() { 
+   tvMode();
+   setTimeout("cicleMidia()",TEMPO_INICIO_MIDIA);
+} 
+
+function cicleMidia() { 
+   setTimeout( function () { 
+	doc = document.getElementById("meio").contentDocument;
+	cc.send( doc.getElementById("galeria").contentDocument, "container", "rotate");
+	cicleMidia();
+   }, TEMPO_REFRESH_MIDIA);
+} 
+
+function tvMode() { 
+  document.getElementById("viewport").style.width="1920";
+  document.getElementById("viewport").style.height="1080";
+  animate();
+} 
+
+function animate() { 
+  tv.add($('#animation li'));
+  tv.play();
+  setTimeout("animate()",TEMPO_REFRESH);
+} 
+
+
