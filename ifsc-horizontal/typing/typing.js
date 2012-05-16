fe65e79a6dd318372e27763f12d3151663298fbb
Marcio Galli <taboca@gmail.com>
2012-05-16 16:34:31 -0300

    fix icon

--- ifsc-horizontal/typing/typing.js
+++ ifsc-horizontal/typing/typing.js
@@ -7,7 +7,7 @@ var typing =  {
 		this.elementTable.innerHTML="<table border='0' width='1900' height='200'><tr><td align='center' valign='middle'><img src='http://olhardigital.uol.com.br/img/logo_olhar_digital.gif' style='margin-left:10px;-moz-border-radius:12px' /></td><td><table border='0' ><tr><td height='190' width='1700' id='basecontainer' valign='middle'><div class='typingPanel' id='typingcontainer'></div></td></tr><tr><td></td></tr></table></td></tr></table>";
 
 		document.getElementById("main").appendChild(this.elementTable);
-		document.getElementById("icon").innerHTML= '';
+		//document.getElementById("icon").innerHTML= '';
 		this.tweetQueue = new Array();
 
 		var first = document.createElement("div");
